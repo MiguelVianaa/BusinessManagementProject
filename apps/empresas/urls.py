7f85bef976a244689b45e7ab6bca1502ecab2307
Miguel <miguelgv00@outlook.com>
2025-05-20 19:40:01 -0300

    Aplicando css e criando algumas telas de empresas

--- apps/empresas/urls.py
+++ apps/empresas/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
-from apps.empresas.views import index
+from apps.empresas.views import index, show, store
 
 urlpatterns = [
     path('empresas', index, name='empresas'),
+    path('empresas/cadastro/', show, name='cadastro'),
+    path('empresas/store', store, name='store'),
 ]
--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Empresa
 
 # Create your views here.
 def index(request):
-    return render(request, 'empresas/index.html')
+    empresas = Empresa.objects.all()
+    return render(request, 'empresas/index.html', {
+        'empresas': empresas
+    })
+def show(request):
+    return render(request, 'empresas/form.html')
+
+def store(request):
+    return render(request, 'empresas/form.html')
