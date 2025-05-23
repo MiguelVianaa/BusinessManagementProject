17dfda0aab7e8aa49ddc315561a88c6118c23f5b
Miguel <miguelgv00@outlook.com>
2025-05-22 22:56:02 -0300

    Adicionando funcionalidades de visualizar e atualizar Empresas

--- apps/empresas/urls.py
+++ apps/empresas/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path
-from apps.empresas.views import index, show, store
+from apps.empresas.views import index, show, store, edit, update
 
 urlpatterns = [
     path('empresas', index, name='empresas'),
     path('empresas/cadastro/', show, name='cadastro'),
     path('empresas/store', store, name='store'),
+    path('empresas/edit/<int:id>/', edit, name='editar'),
+    path('empresas/update/<int:id>/', update, name='update')
 ]
--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,28 +1,71 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.db import transaction
+from django.urls import reverse
 from .models import Empresa
 from .forms import EmpresaForm
+
 
 def index(request):
     return render(request, 'empresas/index.html', {
         'empresas': Empresa.objects.all()
     })
 
+
 def show(request):
-    return render(request, 'empresas/form.html')
+    return render(request, 'empresas/form.html',{
+        'form': EmpresaForm(),
+        'form_action': reverse('store'),
+    })
+
 
 def store(request):
     if request.method  == 'POST':
         form = EmpresaForm(request.POST)
 
         if form.is_valid():
-            empresa = form.save()
+            with transaction.atomic():
+                form.save()
             return redirect('empresas')
         else:
             return render(request, 'empresas/form.html', {
                 'form': form
             })
-    else:
-        form = EmpresaForm()
-        return render(request, 'empresas/form.html', {
-            'form': form
-        })
+
+    form = EmpresaForm()
+    return render(request, 'empresas/form.html', {
+        'form': form
+    })
+
+
+def edit(request, id):
+    empresa = get_object_or_404(Empresa, id=id)
+    form = EmpresaForm(instance=empresa)
+
+    return render(request, 'empresas/form.html', {
+        'form': form,
+        'form_action': reverse('update', args=[id]),
+        'empresa': empresa
+    })
+
+
+def update(request, id):
+    empresa = get_object_or_404(Empresa, id=id)
+
+    if request.method  == 'POST':
+        form = EmpresaForm(request.POST, instance=empresa)
+
+        if form.is_valid():
+            with transaction.atomic():
+                form.save()
+            return redirect('empresas')
+        else:
+            return render(request, 'empresas/form.html', {
+                'form': form,
+                'empresa': empresa
+            })
+
+    form = EmpresaForm(instance=empresa)
+    return render(request, 'empresas/form.html', {
+        'form': form,
+        'empresa': empresa
+    })
